a8945b3a129e7d7527d708463b30fbf6e0c77138
Jiri Sousek <jirisousek@outlook.com>
2019-02-20 16:47:48 +0100

    recursion fixed

--- src/main/java/com/example/clicker/controllers/RestControllers.java
+++ src/main/java/com/example/clicker/controllers/RestControllers.java
@@ -3,6 +3,7 @@ package com.example.clicker.controllers;
 import com.example.clicker.services.ConnectionMethods;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +12,9 @@ public class RestControllers {
     @Autowired
     ConnectionMethods connectionMethods;
 
+    @GetMapping(value = "/")
     public ResponseEntity returnDocument(@RequestBody String url){
-        return ResponseEntity.ok(returnDocument(url));
+        return ResponseEntity.ok(connectionMethods.getDocument(url));
 
     }
 }
