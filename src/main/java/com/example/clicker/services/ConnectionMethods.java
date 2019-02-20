1fef441868f3dd83c16e7f585433f301738ab6de
Jiri Sousek <jirisousek@outlook.com>
2019-02-20 15:50:05 +0100

    jsoup added

---
+++ src/main/java/com/example/clicker/services/ConnectionMethods.java
@@ -0,0 +1,11 @@
+package com.example.clicker.services;
+
+import org.jsoup.nodes.Document;
+
+public class  ConnectionMethods{
+    public Document getDocument (String url){
+        try {
+            Document d =
+        }
+    }
+
