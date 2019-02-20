3037797180d16a8d0613cf8478a8a1411a4989d3
Kopsova <jirina.kopsova@centrum.cz>
2019-02-20 16:49:20 +0100

    m

--- src/main/java/com/example/clicker/controllers/RestControllers.java
+++ src/main/java/com/example/clicker/controllers/RestControllers.java
@@ -3,6 +3,7 @@ package com.example.clicker.controllers;
 import com.example.clicker.services.ConnectionMethods;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ public class RestControllers {
     @Autowired
     ConnectionMethods connectionMethods;
 
+    @GetMapping (value = "/")
     public ResponseEntity returnDocument(@RequestBody String url){
         return ResponseEntity.ok(returnDocument(url));
 
