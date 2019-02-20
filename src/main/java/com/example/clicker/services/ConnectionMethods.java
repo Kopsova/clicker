ad64daa4b194ed28e234359525c9016d50ab8caf
Jiri Sousek <jirisousek@outlook.com>
2019-02-20 16:38:33 +0100

    jsoup method added

--- src/main/java/com/example/clicker/services/ConnectionMethods.java
+++ src/main/java/com/example/clicker/services/ConnectionMethods.java
@@ -1,11 +1,18 @@
 package com.example.clicker.services;
 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class  ConnectionMethods{
     public Document getDocument (String url){
+        Document d = new Document("");
         try {
-            Document d =
+            d = Jsoup.connect(url).get();
         }
+        catch (Exception ex){
+        ex.printStackTrace();
+        }
+    return d;
     }
+}
 
